ddafd7bbb81dcb5b0eb30c7ec309061e09d0430f
Devansh Mamoriya <97148851+devanshmamoriya@users.noreply.github.com>
2026-01-20 23:03:35 +0530

    Initial commit

---
+++ BharatPOS-backend/src/test/java/com/treason/pos/BharatPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treason.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
